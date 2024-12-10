a76a0eef29ff291b1202662746776417dc42d1ca
Vaagai The Vague <vaagai@proton.me>
2024-12-10 06:03:23 +0530

    solved day05 puzzle1

--- days/day_05/main.go
+++ days/day_05/main.go
@@ -2,6 +2,9 @@ package day_05
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -16,10 +19,78 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	orders, page := Parse(input)
+
+	order := map[int][]int{}
+	for _, o := range orders {
+		order[o[0]] = append(order[o[0]], o[1])
+	}
+
+	indices := []int{}
+	for i, p := range page {
+		if IsValidPage(p, order) {
+			indices = append(indices, i)
+		}
+	}
+
+	sum := 0
+	for _, i := range indices {
+		sum += MiddleElement(page[i])
+	}
+
+	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
 	return ""
+}
+
+func Parse(input []string) ([][]int, [][]int) {
+	split := slices.Index(input, "")
+	orderString := input[:split]
+	pageString := input[split+1:]
+
+	orders := [][]int{}
+	for _, line := range orderString {
+		order := strings.Split(line, "|")
+		left, _ := strconv.Atoi(order[0])
+		right, _ := strconv.Atoi(order[1])
+		orders = append(orders, []int{left, right})
+	}
+
+	pages := [][]int{}
+	for _, str := range pageString {
+		p := strings.Split(str, ",")
+
+		numbers := []int{}
+		for _, page := range p {
+			number, _ := strconv.Atoi(page)
+			numbers = append(numbers, number)
+		}
+
+		pages = append(pages, numbers)
+	}
+
+	return orders, pages
+}
+
+func IsValidPage(page []int, order map[int][]int) bool {
+
+	for i, x := range page {
+		if i > 0 {
+			if deps, exists := order[x]; exists {
+				for _, dep := range deps {
+					if dep == page[i-1] {
+						return false
+					}
+				}
+			}
+		}
+	}
+	return true
+}
+
+func MiddleElement(slice []int) int {
+	return slice[len(slice)/2]
 }
